2fb1c4fdfa6a5297fbe701a2c794615a57a9eb7b
jannater <anis_rahim@yahoo.com>
2022-10-02 00:58:14 -0400

    Finalized

--- src/main/java/Basics.java
+++ src/main/java/Basics.java
@@ -43,6 +43,7 @@ public class Basics {
         /* TODO (Task 1): Write a line of code below that prints the string
          *                Hello World!
          */
+        System.out.println("Hello World!");
 
 
 
@@ -62,6 +63,7 @@ public class Basics {
          * TODO (Task 2): Create a variable named my_variable and assign it the
          *                value 100.
          */
+        int my_variable = 100;
 
 
 
@@ -99,6 +101,9 @@ public class Basics {
          * Current count: 1
          * Current count: 0
          */
+        for (int i = 10; i >= 0; i--){
+            System.out.println("Current count: " + i);
+        }
 
 
     }
@@ -143,6 +148,14 @@ public class Basics {
         StringBuilder ret = new StringBuilder();
 
         // Fill in the rest of the body here
+        // Add first letter to ret
+        ret.append(to_split.charAt(0));
+
+        for(int i = 1; i < to_split.length(); i ++){
+            if(to_split.charAt(i) == ' '){
+                ret.append(to_split.charAt((i + 1)));
+            }
+        }
 
         return ret.toString();
     }
@@ -171,6 +184,12 @@ public class Basics {
          *                (e.g. arr[i] gives you the item at index i).
          */
 
+        //Base case: a list with 0 or 1 items
+        for (int i = 0; i < arr.length; i++) {
+            if (i % 2 == 1) {
+                current_sum += arr[i];
+            }
+        }
         return current_sum;
     }
 
